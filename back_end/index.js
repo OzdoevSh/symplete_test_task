6abfa2a8e98353ed6bb97d948ab4ace986fea8e5
Шахид Оздоев <shahidozdoev@MacBook-Pro-Sahid.local>
2020-05-06 22:52:08 +0300

    start commit

---
+++ back_end/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const fib = require('./getFibNum');
+const bodyParser = require('body-parser');
+const mysql = require("mysql");
+
+
+const app = express();
+const port = 5000;
+const jsonParser = bodyParser.json();
+
+const connection = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  database: "testdb",
+  password: "qwerty3129"
+});
+
+connection.connect(function(err){
+    if (err) {
+      return console.error("Ошибка: " + err.message);
+    }
+    else{
+      console.log("Подключение к серверу MySQL успешно установлено");
+    }
+ });
+
+
+app.post('/getNumber', jsonParser, (req, res) => {
+    connection.query("INSERT INTO `testdb`.`success` (`ip`, `index`, `value`) VALUES (?, ?, ?)", [req.ip, req.body.number, fib(req.body.number)]);
+    const gotNumber = req.body.number;
+    const result = fib(gotNumber);
+    res.status(200).json({
+        fibNum: result,
+        number: gotNumber
+    });
+})
+
+
+app.get('/infoget', (req, res) => {
+    connection.query("SELECT * FROM `testdb`.`success`", function (err, inf) {
+        if(err) return console.log(err);
+        else {
+            res.status(200).json({
+                success: inf
+            })
+        console.log(inf)
+        }
+    });    
+})
+
+
+module.exports = app;
+
+app.listen(port);
---
+++ front_end/src/components/getinfo.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+
+class Getinf extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {inf: []}
+    }
+
+
+
+    componentDidMount() {
+        fetch('/infoget')
+          .then(res => res.json())
+          .then(res => this.setState({inf: res.success}, () => console.log("inf fetched..", res)))
+    }
+
+    
+    render(){
+        return (
+            <div>
+                <h1>Информация о запросах</h1>
+                <table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">IP</th>
+                            <th scope="col">Порядковый номер</th>
+                            <th scope="col">Найденное значение</th>
+                            <th scope="col">Дата</th>
+                        </tr>
+                    </thead>
+                    {this.state.inf.map(i =>
+                    <tbody>
+                            <th key={i.id} scope="row">{i.id}</th>
+                            <td>{i.ip}</td>
+                            <td>{i.index}</td>
+                            <td>{i.value}</td>
+                            <td>{moment(i.created_at).format('DD.MM.YYYY HH:mm')}</td>
+                        
+                    </tbody>)}
+                    </table>
+                </div>
+        );
+    }
+
+}
+
+export default Getinf
